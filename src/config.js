10d800c149d238c4a2368145c03e9f2e650b8bf4
thankgoditsfriday.sol <165313569+tgif-sol@users.noreply.github.com>
2024-05-18 16:40:40 -0600

    remove private key from config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ dotenv.config();
 
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 export const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL || "https://api.mainnet-beta.solana.com/";
-export const PRIVATE_KEY = process.env.PRIVATE_KEY;
 export const PUBLIC_ADDRESS = process.env.PUBLIC_ADDRESS;
