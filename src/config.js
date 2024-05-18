4e3878b1c2811fee7e24de70adc6f899e22d0331
thankgoditsfriday.sol <165313569+tgif-sol@users.noreply.github.com>
2024-05-18 16:40:57 -0600

    remove public address from config

--- src/config.js
+++ src/config.js
@@ -3,4 +3,3 @@ dotenv.config();
 
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 export const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL || "https://api.mainnet-beta.solana.com/";
-export const PUBLIC_ADDRESS = process.env.PUBLIC_ADDRESS;
